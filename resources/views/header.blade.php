23496baa29d2016042f4694124b1e946a9a3d41a
lmoboy <steamgoyu19@gmail.com>
2024-05-01 14:14:46 +0300

    modified:   resources/views/header.blade.php 	modified:   routes/web.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,13 +1,13 @@
 <header class="w-screen flex justify-center row">
     <nav class="bg-slate-700 rounded-lg p-5 mx-20">
         <a class="mx-20 rounded-lg p-2 transition-all duration-300 hover:bg-slate-500
-             {{ strpos(Request::url(), '/') === 0 ? 'bg-white' : '' }}"
-            href="/">Home</a>
+             {{ Str::contains(Request::url(), "/home") ? 'bg-white' : '' }}"
+            href="/home">Home</a>
         <a class="mx-20 rounded-lg p-2 transition-all duration-300 hover:bg-slate-500
-             {{ strpos(Request::url(), '/about') === 0 ? 'bg-white' : '' }}"
+             {{ Str::contains(Request::url(), "/about") ? 'bg-white' : '' }}"
             href="/about">About</a>
         <a class="mx-20 rounded-lg p-2 transition-all duration-300 hover:bg-slate-500
-             {{ strpos(Request::url(), '/contact') === 0 ? 'bg-white' : '' }}"
+             {{ Str::contains(Request::url(), "/contact") ? 'bg-white' : '' }}"
             href="/contact">Contact</a>
     </nav>
 </header>
